85da62a44a3bf9d74c83b81a18d6720068f86d1d
Jose Gonzalez <myaccounts@savant.be>
2009-08-01 20:02:42 -0400

    somehow reverted to non-task based shell

--- vendors/shells/github_plugin.php
+++ vendors/shells/github_plugin.php
@@ -1,5 +1,4 @@
 <?php
-App::import(array('HttpSocket', 'File', 'Folder', 'Xml'));
 /**
  * Github Plugin management shell.
  *
@@ -8,11 +7,11 @@ App::import(array('HttpSocket', 'File', 'Folder', 'Xml'));
 class GithubPluginShell extends Shell {
 
 /**
- * HttpSocket instance.
+ * Tasks that are attached to this shell
+ *
  * @var string
- */
-	var $Socket = null;
-
+ **/
+	var $tasks = array('List', 'Find', 'Install', 'Update');
 /**
  * Main shell logic.
  *
@@ -25,8 +24,6 @@ class GithubPluginShell extends Shell {
 			$this->serverUri = $this->params['server'];
 			$this->out('Using specified server: ' . $this->serverUri);
 		}
-
-		$this->Socket = new HttpSocket();
 
 		if(!empty($this->command)) {
 			$this->command = substr($this->command, 0, 1);
@@ -42,18 +39,21 @@ class GithubPluginShell extends Shell {
  * @author Jose Diaz-Gonzalez
  */
 	function __run() {
-		$validCommands = array('l', 'v', 's', 'g', 'z', 'p', 'u', 'q');
+		$githubUser = 'cakephp-plugin-provider';
+
+		$validCommands = array('l', 'v', 'f', 'g', 'z', 'p', 'u', 'q');
 
 		while (empty($this->command)) {
 			$this->out("Github Plugin Server");
 			$this->hr();
 			$this->out("[L]ist Installed Plugins");
 			$this->out("[V]iew Available Plugins");
-			$this->out("[S]earch Available Plugins");
+			$this->out("[F]ind a specific Plugin");
 			$this->out("[G]it Install Plugin as Submodule");
 			$this->out("[Z]ip Install Plugin from an archive");
 			$this->out("[P]ull all Plugin Submodule Updates");
 			$this->out("[U]pdate a specific Plugin Submodule");
+			$this->out("[C]ache Configuration");
 			$this->out("[Q]uit");
 			$temp = $this->in("What command would you like to perform?", $validCommands, 'i');
 			if (in_array(strtolower($temp), $validCommands)) {
@@ -65,26 +65,28 @@ class GithubPluginShell extends Shell {
 
 		switch ($this->command) {
 			case 'l' :
-				$this->__doList();
+				$this->List->installed();
 				break;
 			case 'v' :
-				$this->__doView();
+				$this->List->available($githubUser);
 				break;
-			case 's' :
-				$this->__doSearch();
+			case 'f' :
+				$this->Find->execute();
 				break;
 			case 'g' :
-				$this->__doSubmoduleInstall();
+				$this->Install->git($githubUser);
 				break;
 			case 'z' :
-				$this->__doZipInstall();
+				$this->Install->zip($githubUser);
 				break;
 			case 'p' :
-				$this->__doPull();
+				$this->Update->all();
 				break;
 			case 'u' :
-				$this->__doUpdate();
+				$this->Update->specific();
 				break;
+			case 'c' :
+				$this->Cache->execute();
 			case 'q' :
 				$this->out(__("Exit", true));
 				$this->_stop();
